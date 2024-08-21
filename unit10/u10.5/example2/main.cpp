5d65123ecf099b5091a67a04f2904b8977dedaf9
Ajay Unnikrishnan <ajayu.canada@gmail.com>
2024-08-21 17:25:08 +0530

    completed sect 10.5

---
+++ unit10/u10.5/example2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <typeinfo> // for typeid()
+
+int main()
+{
+    short a{ 4 };
+    short b{ 5 };
+    std::cout << typeid(a + b).name() << ' ' << a + b << '\n'; // show us the type of a + b
+
+    return 0;
+}
---
+++ unit10/u10.5/example3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <typeinfo> // for typeid()
+
+int main()
+{
+    std::cout << typeid(5u-10).name() << ' ' << 5u - 10 << '\n'; // 5u means treat 5 as an unsigned integer
+
+    return 0;
+}
