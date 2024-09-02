360c3cd4660852b4a719c929d5f7fdc4fd3e48f0
Ajay Unnikrishnan <ajayu.canada@gmail.com>
2024-09-01 19:21:49 -0700

    completed sect 11.10

---
+++ unit11/u11.10/example1/main.cpp
@@ -0,0 +1,8 @@
+#include <bitset>
+
+int main()
+{
+    std::bitset<8> bits{ 0b0000'0101 }; // The <8> is a non-type template parameter
+
+    return 0;
+}
---
+++ unit11/u11.10/example7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <auto N> // deduce non-type template parameter from template argument
+void print()
+{
+    std::cout << N << '\n';
+}
+
+int main()
+{
+    print<5>();   // N deduced as int `5`
+    print<'c'>(); // N deduced as char `c`
+
+    return 0;
+}
